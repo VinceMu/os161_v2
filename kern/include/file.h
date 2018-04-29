5212bb6266a19cc795ba362805203e2384d858da
z5115087 <z5115087@cse.unsw.edu.au>
2018-04-29 20:38:55 +1000

    added function prototypes in file.h

--- kern/include/file.h
+++ kern/include/file.h
@@ -10,6 +10,18 @@
  */
 #include <limits.h>
 
+//variable file length
+int open(const char *filename, int flags,...);
+
+ssize_t read(int fd, void *buf, size_t buflen);
+
+ssize_t write(int fd, const void *buf, size_t nbytes);
+
+off_t lseek(int fd, off_t pos, int whence);
+
+int close(int fd);
+
+int dup2(int oldfd, int newfd);
 
 /*
  * Put your function declarations and data types here ...
--- kern/syscall/file.c
+++ kern/syscall/file.c
@@ -19,3 +19,4 @@
  * Add your file-related functions here ...
  */
 
+void 
