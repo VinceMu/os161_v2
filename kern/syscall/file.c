66353a4722f1335d6f0c7f1cd1adf6ad33f8ad98
z5115087 <z5115087@cse.unsw.edu.au>
2018-04-29 21:50:11 +1000

    working on open

--- kern/syscall/file.c
+++ kern/syscall/file.c
@@ -19,4 +19,23 @@
  * Add your file-related functions here ...
  */
 
-void 
+int open(const char *filename,int flags,mode_t mode,int *)
+{
+   struct vnode *file_node; 	
+   //intialise file and get response code
+   int response = vfs_open(filename,flags,mode,&file_node);
+   if(response)
+   {
+      return response;
+   }
+   struct lock *file_lock = lock_create("file_open_lock"); 
+   if(struct file_lock == NULL)//could not create lock 
+   {
+      vfs_close(file_node);
+      return ENOMEM; //return not enough memory error 
+   }
+   /**
+   not finished!!!!
+   */
+   return 0;
+} 
